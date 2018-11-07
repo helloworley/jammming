81fd3b27d2e96d35f6cf605863656bb54e1feab2
Joshua Worley <helloworley@gmail.com>
2018-11-08 06:22:13 +0900

    Added search results and new playlist data as arrays in the state

--- src/App.js
+++ src/App.js
@@ -4,25 +4,36 @@ import './App.css';
 import SearchBar from './components/Searchbar/Searchbar';
 import TrackList from './components/TrackList/TrackList';
 
-const tracks = [
-  {
-    title: 'Mountaintops In Caves',
-    artist: 'Talkdemonic',
-    album: 'Beat Romantic'
-  },
-  {
-    title: 'Chihiro',
-    artist: 'Yoste',
-    album: 'Chihiro'
-  },
-  {
-    title: 'Northern Lights',
-    artist: 'Lights & Motion',
-    album: 'Chronicle'
-  }
-]
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResultTracks: [
+        {
+          title: 'Mountaintops In Caves',
+          artist: 'Talkdemonic',
+          album: 'Beat Romantic'
+        },
+        {
+          title: 'Chihiro',
+          artist: 'Yoste',
+          album: 'Chihiro'
+        },
+        {
+          title: 'Northern Lights',
+          artist: 'Lights & Motion',
+          album: 'Chronicle'
+        }
+      ],
+      newPlayList: [
+
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -30,11 +41,11 @@ class App extends Component {
         <div class="App-playlist">
           <div class="SearchResults">
             <h2>Results</h2>
-            <TrackList tracks={tracks} />
+            <TrackList tracks={this.state.searchResultTracks} />
           </div>
           <div class="Playlist">
             <input value='New Playlist' />
-            <TrackList tracks={tracks} />
+            <TrackList tracks={this.state.newPlayList} />
             <a class="Playlist-save">SAVE TO SPOTIFY</a>
           </div>
         </div>
